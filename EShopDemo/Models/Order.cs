caac33ff24e732b26ddc0f98d670252cd94ce437
tarifSe <gmtariful@gmail.com>
2022-04-18 18:37:26 +0600

    WorkingOnChkOut

--- EShopDemo/Models/Order.cs
+++ EShopDemo/Models/Order.cs
@@ -17,7 +17,9 @@ namespace EShopDemo.Models
         [Required]
         [Display(Name = "Phone No")]
         public string PhoneNo { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Address { get; set; }
         public DateTime OrderDate { get; set; }
         public virtual List<OrderDetails> OrderDetails { get; set; }
