ffdbc7615d23490b53e5c5607b114afc270a4e43
tarifSe <gmtariful@gmail.com>
2022-04-18 16:17:51 +0600

    OrderControllerAdded

---
+++ EShopDemo/Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,56 @@
+﻿using EShopDemo.Data;
+using EShopDemo.Models;
+using EShopDemo.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShopDemo.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public OrderController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Checkout(Order anOrder)
+        {
+            List<Products> products = HttpContext.Session.Get<List<Products>>("products");
+            if (products != null)
+            {
+                foreach (var item in products)
+                {
+                    OrderDetails orderDetails = new OrderDetails() 
+                    { 
+                        ProductsId = item.Id 
+                    };
+                    //orderDetails.ProductsId = item.Id;
+                    anOrder.OrderDetails.Add(orderDetails);
+                }
+            }
+            anOrder.OrderNo = GetOrderNo();
+            _context.Orders.Add(anOrder);
+            await _context.SaveChangesAsync();
+            HttpContext.Session.Set("products", null);
+            return View();
+        }
+
+        private string GetOrderNo()
+        {
+            int count = _context.Orders.ToList().Count() + 1;
+            return count.ToString("000");
+        }
+    }
+}
