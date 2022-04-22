3315ea73f01c96c8ea4d2f731d665fb933ad3df5
tarifSe <gmtariful@gmail.com>
2022-04-22 16:03:51 +0600

    NewMigrationAddedIntoUserTbl

--- EShopDemo/Data/Migrations/20220422095613_AddedNewFieldIntoUserTbl.Designer.cs
+++ EShopDemo/Data/Migrations/20220422095613_AddedNewFieldIntoUserTbl.Designer.cs
@@ -4,14 +4,16 @@ using EShopDemo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EShopDemo.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220422095613_AddedNewFieldIntoUserTbl")]
+    partial class AddedNewFieldIntoUserTbl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EShopDemo/Data/Migrations/20220422095613_AddedNewFieldIntoUserTbl.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EShopDemo.Data.Migrations
+{
+    public partial class AddedNewFieldIntoUserTbl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Orders",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Orders",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
