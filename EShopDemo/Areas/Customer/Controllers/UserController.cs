1a25ef2461fc59cc4cff7f40e849be0f3be937e7
tarifSe <gmtariful@gmail.com>
2022-04-24 17:51:34 +0600

    UserAddView_Added

--- EShopDemo/Areas/Customer/Controllers/UserController.cs
+++ EShopDemo/Areas/Customer/Controllers/UserController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace EShopDemo.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class UserController : Controller
     {
         public IActionResult Index()
