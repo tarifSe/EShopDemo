8601ce282016d80d0352e824b319d5c3f2f2bea4
tarifSe <gmtariful@gmail.com>
2022-04-25 23:32:11 +0600

    UserSavingProccessDone

--- EShopDemo/Areas/Customer/Controllers/UserController.cs
+++ EShopDemo/Areas/Customer/Controllers/UserController.cs
@@ -3,12 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EShopDemo.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace EShopDemo.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class UserController : Controller
     {
+        UserManager<IdentityUser> _userManager;
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +24,22 @@ namespace EShopDemo.Areas.Customer.Controllers
 
         public async Task<IActionResult> Create()
         {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ApplicationUser appUser)
+        {
+            var result = await _userManager.CreateAsync(appUser);
+            if (result.Succeeded)
+            {
+
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
             return View();
         }
     }
