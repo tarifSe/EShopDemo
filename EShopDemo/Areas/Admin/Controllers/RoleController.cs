c41bc362ae21d0d21c565ff38828d8c48e1bdf24
tarifSe <gmtariful@gmail.com>
2022-05-09 16:44:17 +0600

    RoleController_Added

---
+++ EShopDemo/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShopDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        RoleManager<IdentityUser> _roleManager;
+        public RoleController(RoleManager<IdentityUser> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
